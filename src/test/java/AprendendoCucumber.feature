f61d1eae38c616b0282c6bf8e38f6df09fcc039f
Teofilo Messias Filho <re045002@qintess.com>
2024-05-15 15:27:15 -0300

    Seção 2 - Fundamentos: Aprendendo Regex

--- src/test/java/AprendendoCucumber.feature
+++ src/test/java/AprendendoCucumber.feature
@@ -13,3 +13,36 @@ Cenario: Deve inclementar contador
 	Dado que o valor do contador é 15
 	Quando eu incrementar em 3
 	Entao o valor do contador será 18
+
+Cenario: Deve inclementar contador
+	Dado que o valor do contador é 123
+	Quando eu incrementar em 35
+	Entao o valor do contador será 158
+	
+Cenario: Deve calcular atraso na entrega
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 dias
+	Entao a entrega será efetuada em 07/04/2018
+	
+Cenario: Deve calcular atraso no prazo de entrega da China
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 meses
+	Entao a entrega será efetuada em 05/06/2018
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,4 +1,11 @@
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -30,12 +37,51 @@ public class AprenderCucumber {
     	contador = contador + arg1;
     }
 
-    @Entao("^o valor do contador será (\\d+)$")
+    @Entao("^o valor do contador será (\\d+)$") // expressões regulares (\\d+)$
     public void oValorDoContadorSer(int arg1) throws Throwable {
-    	System.out.println(arg1);
-    	System.out.println(contador);
-    	System.out.println(arg1 == contador);
-       
+//    	https://jex.im/regulex
+//		https://regex101.com/    	
+//    	System.out.println(arg1);
+//    	System.out.println(contador);
+//    	System.out.println(arg1 == contador);
+//    	Assert.assertTrue(arg1 == contador);
+    	Assert.assertEquals(arg1,contador);
+//		throw new RuntimeException();
+    	
+    }
+    Date entrega = new Date();
+    
+    @Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+    public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+    	Calendar cal = Calendar.getInstance();
+    	cal.set(Calendar.DAY_OF_MONTH, dia);
+    	cal.set(Calendar.MONTH, mes - 1);
+    	cal.set(Calendar.YEAR, ano);
+    	entrega = cal.getTime();
+        
+    }
+
+    @Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+    public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+    	Calendar cal = Calendar.getInstance();
+    	
+    	cal.setTime(entrega);
+    	if(tempo.equals("dias")) { 	
+    	    cal.add(Calendar.DAY_OF_MONTH, arg1);
+    	}
+    	if(tempo.equals("meses")) {
+    		cal.add(Calendar.MONTH, arg1);
+    	}
+    	entrega = cal.getTime();
+    	
+    }
+
+    @Entao("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+    	DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    	String dataFormatada = format.format(entrega);
+    	Assert.assertEquals(data,dataFormatada);
+        
     }
 
 }
