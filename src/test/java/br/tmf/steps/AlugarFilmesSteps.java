b9058820376add458e482de1bff5681a8daedc7e
Teofilo Messias Filho <re045002@qintess.com>
2024-05-27 17:05:59 -0300

    Seção 3 - Testes Data-Driven

--- src/test/java/br/tmf/steps/AlugarFilmesSteps.java
+++ src/test/java/br/tmf/steps/AlugarFilmesSteps.java
@@ -13,6 +13,7 @@ import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 import entidades.Filme;
 import entidades.NotaAluguel;
+import entidades.TipoAluguel;
 import servicos.AluguelService;
 import utils.DateUtils;
 
@@ -21,7 +22,7 @@ public class AlugarFilmesSteps {
 	private AluguelService aluguel = new AluguelService() ;
 	private NotaAluguel nota;
 	private String erro;
-	private String tipoAluguel;
+	private TipoAluguel tipoAluguel = TipoAluguel.COMUM;
 	
 	@Dado("^um filme com estoque de (\\d+) unidades$")
 	public void umFilmeComEstoqueDeUnidades(int arg1) throws Throwable {
@@ -80,7 +81,7 @@ public class AlugarFilmesSteps {
 	}
 	@Dado("^que o tipo do aluguel seja (.*)$")
 	public void queOTipoDoAluguelSejaExtendido(String tipo) throws Throwable {
-	   tipoAluguel = tipo;
+	   tipoAluguel = tipo.equals("semanal")? TipoAluguel.SEMANAL: tipo.equals("extendido")? TipoAluguel.EXTENDIDO: tipoAluguel.COMUM;
 	}
 	@Então("^a data de entrega será em (\\d+) dias?$")
 	public void aDataDeEntregaSeráEmDias(int arg1) throws Throwable {
